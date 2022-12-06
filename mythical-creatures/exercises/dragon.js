3313f7769adf8fd82ce8d7c16b7b9aaf4d78f369
Trey Marshall <marshall.trey92@yahoo.com>
2022-12-06 16:03:44 -0600

    Add work on Unicorn and Dragon

--- mythical-creatures/exercises/dragon.js
+++ mythical-creatures/exercises/dragon.js
@@ -1,1 +1,23 @@
+class Dragon {
+    constructor (name, rider, hungry) {
+        this.name = name;
+        this.rider = rider;
+        this.hungry = hungry; {
+            if (hungry === hungry) {
+                this.hungry = true
+            }
+        }
+    }
 
+    greet() {
+        return `Hi, ${this.rider}!`
+    }
+    eat() {
+        if (this.hungry === true) {
+            return false
+        }
+    }
+}
+
+
+module.exports = Dragon;
